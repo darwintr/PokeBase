244b9494e639a0091c30ce513cbc84f59ef221e5
darwin.truong@outlook.com <darwin.truong@outlook.com>
2017-06-24 23:31:34 -0400

    something

---
+++ app/src/main/java/com/example/darwin/pokebase/Pokemon.java
@@ -0,0 +1,19 @@
+package com.example.darwin.pokebase;
+
+/**
+ * Created by Darwin on 6/24/2017.
+ */
+
+public class Pokemon {
+	private String name;
+	private String dexNum;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDexNum() {
+		return dexNum;
+	}
+
+}
